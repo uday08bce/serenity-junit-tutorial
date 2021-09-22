facd76d35cbf3f7fbaee8c41b06c8fca0697cbaa
Uday <uday08bce@gmail.com>
2021-09-22 22:28:15 +0200

    Added UIInteraction and PageObject feature of serenity .

--- src/test/java/serenityswag/authentication/WhenLoggingOn.java
+++ src/test/java/serenityswag/authentication/WhenLoggingOn.java
@@ -1,32 +1,36 @@
 package serenityswag.authentication;
 
+import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import serenityswag.authentication.actions.LoginActions;
+import serenityswag.inventory.InventoryPage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 //import org.junit.runner.Runwith;
 
 @RunWith(SerenityRunner.class)
-public class WhenLoggingOn {
+public class WhenLoggingOn extends UIInteractionSteps {
 
     @Managed
     WebDriver driver;
 
+    @Steps
+    LoginActions login;
+
+    InventoryPage inventory;
     @Test
     public void usersCanLogOnViaTheHomepage(){
-        driver.get("https://www.saucedemo.com/");
 
-        //Login as a standard user
-        driver.findElement(By.cssSelector("[data-test='username']")).sendKeys("standard_user");
-        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector("[data-test='login-button']")).click();
+        login.loginAsAStandarduser();
 
-        //should see product cataloge
-        assertThat(driver.findElement(By.cssSelector(".title")).getText()).
+
+        assertThat(inventory.getHeading()).
                 isEqualToIgnoringCase("products");
     }
 }
